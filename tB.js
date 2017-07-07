b5b7a1b9814933ed228a394cb8456d49ac723c59
Taylor Bietila <taylor@traffichaus.com>
2017-07-07 10:08:07 -0700

    changes to portfolio site

---
+++ tB.js
@@ -0,0 +1,46 @@
+(() => {
+  'use strict';
+ 
+    //DOM manipulation code here
+		let refOffset = 0;
+		let visible = true;
+		const bannerHeight = 100;
+		const bannerWrapper = document.querySelector('.nav-bar');
+		const banner = document.querySelector('.banner');
+
+		function animateOut() {
+		  bannerWrapper.style.transform = `translateY(-${bannerHeight}px)`;
+		  bannerWrapper.style.msTransform = `translateY(-${bannerHeight}px)`;
+		  bannerWrapper.style.webkitTransform = `translateY(-${bannerHeight}px)`;
+		  bannerWrapper.style.MozTransform = `translateY(-${bannerHeight}px)`;
+		  banner.style.background = 'rgba(255, 255, 255, 0.6)';
+		}
+
+		function animateIn() {
+		  bannerWrapper.style.transform = 'translateY(0px)';
+		  bannerWrapper.style.msTransform = 'translateY(0px)';
+		  bannerWrapper.style.webkitTransform = 'translateY(0px)';
+		  bannerWrapper.style.MozTransform = 'translateY(0px)';
+		  banner.style.background = 'rgba(255, 255, 255, 0.6)';
+		}
+
+		const handler = () => {
+		const newOffset = window.scrollY || window.pageYOffset;
+			
+			if (newOffset > bannerHeight) {
+			  if (newOffset > refOffset) {
+			    animateOut();
+			  } else {
+			    animateIn();
+			  }
+
+			  refOffset = newOffset;
+			} else {
+			  banner.style.backgroundColor = 'rgba(255, 255, 255, 1)';
+			}
+
+		
+		  };
+
+  	window.addEventListener('scroll', handler, false);
+})();
